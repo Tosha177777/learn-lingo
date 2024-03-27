e6f2b21bed774d67b754739586bf2f32965eef3d
Tosha177777 <73199654+Tosha177777@users.noreply.github.com>
2024-03-27 20:07:36 +0200

    login modal

---
+++ src/components/Login/LoginModal.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect } from 'react';
+import * as yup from 'yup';
+import './LoginStyle.scss';
+import { Field, Form, Formik } from 'formik';
+
+const SignupSchema = yup.object().shape({
+  email: yup.string().email('Invalid email').required('Required'),
+  password: yup
+    .string()
+    .min(7, 'Too Short!')
+    .max(30, 'Too Long!')
+    .required('Required'),
+});
+
+const LoginModal = ({ onClose }) => {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
+  const handleClick = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  };
+
+  return (
+    <div className="overlay" onClick={handleClick}>
+      <div className="modal">
+        <h3 className="loginTitle">Log In</h3>
+        <p className="welcome">
+          Welcome back! Please enter your credentials to access your account and
+          continue your search for an teacher.
+        </p>
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+          }}
+          validationSchema={SignupSchema}
+          onSubmit={values => {
+            // same shape as initial values
+            console.log(values);
+          }}
+        >
+          {({ errors, touched }) => (
+            <Form className="form">
+              <Field
+                name="email"
+                type="email"
+                className="field"
+                placeholder="Email"
+              />
+              {errors.email && touched.email ? <div>{errors.email}</div> : null}
+              <Field
+                name="password"
+                type="password"
+                className="field"
+                placeholder="Password"
+                pattern=".{7,}"
+              />
+              {errors.password && touched.password ? (
+                <div>{errors.password}</div>
+              ) : null}
+              <button type="submit" className="logBtn">
+                Log In
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+};
+
+export default LoginModal;
